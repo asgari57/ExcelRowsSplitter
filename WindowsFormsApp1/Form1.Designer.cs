65910b769bc5bd1e372c1ba92b120dc5bdd4d034
asgari57 <r.asgari94@gmail.com>
2021-05-07 03:08:50 +0430

    ver1

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.txtSave = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.nRows = new System.Windows.Forms.Label();
+            this.nCoulmn = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtRows = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(509, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = ".xslx file";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(89, 16);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(387, 22);
+            this.txtFile.TabIndex = 2;
+            // 
+            // txtSave
+            // 
+            this.txtSave.Location = new System.Drawing.Point(89, 214);
+            this.txtSave.Name = "txtSave";
+            this.txtSave.Size = new System.Drawing.Size(387, 22);
+            this.txtSave.TabIndex = 5;
+            this.txtSave.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Save to";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(509, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Rows";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(197, 259);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Separate to multiple Files";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // nRows
+            // 
+            this.nRows.AutoSize = true;
+            this.nRows.Location = new System.Drawing.Point(134, 86);
+            this.nRows.Name = "nRows";
+            this.nRows.Size = new System.Drawing.Size(31, 17);
+            this.nRows.TabIndex = 8;
+            this.nRows.Text = "N/A";
+            // 
+            // nCoulmn
+            // 
+            this.nCoulmn.AutoSize = true;
+            this.nCoulmn.Location = new System.Drawing.Point(414, 86);
+            this.nCoulmn.Name = "nCoulmn";
+            this.nCoulmn.Size = new System.Drawing.Size(31, 17);
+            this.nCoulmn.TabIndex = 10;
+            this.nCoulmn.Text = "N/A";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(353, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Column";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(211, 44);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(171, 34);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Check file";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(193, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "How many rows in each files?";
+            // 
+            // txtRows
+            // 
+            this.txtRows.Location = new System.Drawing.Point(211, 154);
+            this.txtRows.Name = "txtRows";
+            this.txtRows.Size = new System.Drawing.Size(84, 22);
+            this.txtRows.TabIndex = 18;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Excel Files |*.xlsx";
+            this.openFileDialog1.ReadOnlyChecked = true;
+            this.openFileDialog1.ShowHelp = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 331);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(566, 25);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 19;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(356, 155);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(186, 21);
+            this.checkBox1.TabIndex = 20;
+            this.checkBox1.Text = "is your file have header?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(203, 311);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(0, 17);
+            this.lblTimer.TabIndex = 21;
+            this.lblTimer.Click += new System.EventHandler(this.lblTimer_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkVisited = true;
+            this.linkLabel1.Location = new System.Drawing.Point(203, 306);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(179, 17);
+            this.linkLabel1.TabIndex = 22;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Developed for asiahkz.com";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 368);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtRows);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.nCoulmn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.nRows);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Excel file spliter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.TextBox txtSave;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label nRows;
+        private System.Windows.Forms.Label nCoulmn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtRows;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;        //Microsoft Excel 14 object in references-> COM tab
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static int rowCount;
+        public static int  colCount;
+        public int sec = 0;
+        public int min = 0;
+        public int hr = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog()==DialogResult.OK)
+
+            txtSave.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // openFileDialog1.ShowDialog();
+            // txtFile.Text = openFileDialog1.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFile.Text = openFileDialog1.FileName;
+
+
+            }
+
+
+
+        }
+
+
+       
+       
+
+       
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(txtFile.Text);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            rowCount = xlRange.Rows.Count;
+            colCount = xlRange.Columns.Count;
+
+
+
+            nRows.Text = rowCount.ToString();
+            nCoulmn.Text = colCount.ToString();
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:
+            //  never use two dots, all COM objects must be referenced and released individually
+            //  ex: [somthing].[something].[something] is bad
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(txtFile.Text);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            rowCount = xlRange.Rows.Count;
+            colCount = xlRange.Columns.Count;
+            int number;
+          
+            int c = 0;
+            int cc = 0;
+            bool isParsable = Int32.TryParse(txtRows.Text, out number);
+            if (isParsable)
+                c = number;
+            object misValue = System.Reflection.Missing.Value;
+
+            Excel.Workbook xlWorkBook2;
+            Excel.Worksheet xlWorkSheet2;
+            xlWorkBook2 = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet2 = (Excel.Worksheet)xlWorkBook2.Worksheets.get_Item(1);
+
+            int nfile = 0;
+
+
+            progressBar1.Maximum = rowCount;
+            progressBar1.Step = 1;
+
+          //  int r = 0;
+            for (int x = 1; x <= colCount; x++)
+            {
+                xlWorkSheet2.Cells[1, x] = xlWorksheet.Cells[1, x];
+
+            }
+
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    if (cc <= c && i<=c )
+                    {
+                        xlWorkSheet2.Cells[i-cc, j] = xlWorksheet.Cells[i, j];
+                        
+                        if (i-cc == 1 && checkBox1.Checked) { 
+                        xlWorkSheet2.Cells[1, j] = xlWorksheet.Cells[1,j];
+                        }
+                    }
+                }
+                if (i == c && c <= rowCount)
+                {
+                    // r = 0;
+                    cc = c;
+                    c = c + c;
+
+                    
+                    
+                    nfile += 1;
+                    xlWorkBook2.SaveAs(txtSave.Text + "\\split" + nfile + ".xlsx", Excel.XlFileFormat.xlWorkbookDefault, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                    xlWorkBook2.Close(true, misValue, misValue);
+                    xlWorkBook2 = xlApp.Workbooks.Add(misValue);
+                    xlWorkSheet2 = (Excel.Worksheet)xlWorkBook2.Worksheets.get_Item(1);
+
+                    
+                    
+                }
+
+                
+                progressBar1.PerformStep();
+                
+            }
+
+            nfile += 1;
+            xlWorkBook2.SaveAs(txtSave.Text + "\\split" + nfile + ".xlsx", Excel.XlFileFormat.xlWorkbookDefault, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook2.Close(true, misValue, misValue);
+            Marshal.ReleaseComObject(xlWorkSheet2);
+            Marshal.ReleaseComObject(xlWorkBook2);
+
+
+
+
+
+
+
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:
+            //  never use two dots, all COM objects must be referenced and released individually
+            //  ex: [somthing].[something].[something] is bad
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+
+
+
+            MessageBox.Show("Finished!", "Process information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            progressBar1.Value = 0;
+          
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void lblTimer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //sec++;
+            //if (sec == 60)
+            //{
+            //    min++;
+            //    sec = 0;
+            //}
+            //if (min == 60)
+            //{
+            //    hr++;
+            //    min = 0;
+            //    sec = 0;
+            //}
+
+
+            //lblTimer.Text = hr.ToString() + ":" + min.ToString() + ":" + sec.ToString();
+
+
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://www.asiahkz.com");
+
+        }
+    }
+}
